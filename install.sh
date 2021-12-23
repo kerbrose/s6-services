dceec9d76c231b909560d5cf1bda8f79fc12aba6
Dudemanguy <dudemanguy@artixlinux.org>
2021-12-22 22:46:12 -0600

    update to new s6-rc format
    Also make the install.sh script /bin/sh (it's POSIX) and touch up some
    quoting and stuff.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 PKG="$1"
 
@@ -7,19 +7,28 @@ DESTDIR="$2"
 SVDIR="${DESTDIR}"/etc/s6/sv
 CONFDIR="${DESTDIR}"/etc/s6/config
 
-for dir in $PKG/*; do
-    if [ -d $dir ]; then
-        dirname=$(basename $dir)
-        install -v -d "${SVDIR}/$dirname"
-        for file in $dir/*; do
-            install -v -m644 "$file" ${SVDIR}/$dirname
+for dir in "$PKG"/*; do
+    if [ -d "$dir" ]; then
+        dirname=$(basename "$dir")
+        for subdir in "$dir"/*; do
+            if [ -d "$subdir" ]; then
+                subdirname=$(basename "$subdir")
+                install -v -d "${SVDIR}"/"$dirname"/"$subdirname"
+                for file in "$subdir"/*; do
+                    install -v -m644 "$file" "${SVDIR}"/"$dirname"/"$subdirname"
+                done
+            fi
+        done
+        install -v -d "${SVDIR}"/"$dirname"
+        for file in "$dir"/*; do
+            install -v -m644 "$file" "${SVDIR}"/"$dirname"
         done
     fi
 done
 
-if [ -e $PKG/*.conf ]; then
-    for conf in $PKG/*.conf; do
+if [ -e "$PKG"/*.conf ]; then
+    for conf in "$PKG"/*.conf; do
         install -v -d "${CONFDIR}"
-        install -v -m644 "$conf" ${CONFDIR}/$PKG.conf
+        install -v -m644 "$conf" "${CONFDIR}"/"$PKG".conf
     done
 fi
